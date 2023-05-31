7ed984f60e1c9b89bd3ce00fc668b9d02ac58480
TJBaumgartner <tnorth6@gmail.com>
2023-05-31 12:57:53 -0700

    project setup

---
+++ src/test.test.js
@@ -0,0 +1,6 @@
+// const myModule = require('./myModule');
+// const capitalize = myModule.capitalize;
+// const reverseString = myModule.reverseString;
+// const calculator = myModule.calculator;
+// const ceaserChiper = myModule.ceaserChiper;
+// const analyzeArray = myModule.analyzeArray;
